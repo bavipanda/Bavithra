2c0ec4ffc3d112b2e2642dc37dc4300003acd22c
bavipanda <52281228+bavipanda@users.noreply.github.com>
2019-07-20 12:28:15 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,9 @@
+c=input()
+ 
+if((c>='a' and c<='z') or (c>='A' and c<='Z')):
+  if(c=='a' or c=='A' or c=='e' or c=='E' or c=='i' or c=='I' or c=='o' or c=='O' or c=='u' or c=='U'):
+    print("Vowel")
+  else:
+    print("Consonant")
+else:
+  print("invalid")
