84b3f524216c519400413d844a9241b367afd4ed
Bavithra <52281228+bavipanda@users.noreply.github.com>
2020-01-19 18:54:50 +0530

    Create  Sum of Digits of a Number using While Loop.py

---
+++ Sum of Digits of a Number using While Loop.py
@@ -0,0 +1,9 @@
+Number = int(input())
+Sum = 0
+
+while(Number > 0):
+    Reminder = Number % 10
+    Sum = Sum + Reminder
+    Number = Number //10
+
+print("\n Sum of the digits of Given Number = %d" %Sum)
