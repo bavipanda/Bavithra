1a00eb97c802efae1cff53b888d22691936760c4
Bavithra <52281228+bavipanda@users.noreply.github.com>
2020-01-19 18:59:20 +0530

    Create Product of digits in a number.py

---
+++ Product of digits in a number.py
@@ -0,0 +1,11 @@
+num = int(input())
+
+temp = num
+product = 1;
+
+while(temp != 0):
+
+    product = product * (temp % 10);
+    temp = int(temp / 10)
+
+print("\nProduct of all digits in", num, ":", product)
